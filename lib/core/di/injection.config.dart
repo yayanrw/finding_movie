9f3860e0327361681e70e6445c4af35674c67758
yayanrw <yayanraw@gmail.com>
2023-05-11 06:52:17 +0700

    add generated files

---
+++ lib/core/di/injection.config.dart
@@ -0,0 +1,37 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+// ignore_for_file: unnecessary_lambdas
+// ignore_for_file: lines_longer_than_80_chars
+// coverage:ignore-file
+
+// ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:get_it/get_it.dart' as _i1;
+import 'package:http/http.dart' as _i4;
+import 'package:injectable/injectable.dart' as _i2;
+
+import '../router/router.dart' as _i3;
+import 'register_module.dart' as _i5;
+
+extension GetItInjectableX on _i1.GetIt {
+  // initializes the registration of main-scope dependencies inside of GetIt
+  _i1.GetIt init({
+    String? environment,
+    _i2.EnvironmentFilter? environmentFilter,
+  }) {
+    final gh = _i2.GetItHelper(
+      this,
+      environment,
+      environmentFilter,
+    );
+    final registerModule = _$RegisterModule();
+    gh.singleton<_i3.AppRouter>(registerModule.appRouter);
+    gh.lazySingleton<_i4.Client>(() => registerModule.httpClient);
+    return this;
+  }
+}
+
+class _$RegisterModule extends _i5.RegisterModule {}
