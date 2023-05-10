673b496adf2e904c16b9ca731e8424fe6f0f353a
yayanrw <yayanraw@gmail.com>
2023-05-11 06:52:08 +0700

    add router.dart

---
+++ lib/core/router/router.dart
@@ -0,0 +1,10 @@
+import 'package:auto_route/annotations.dart';
+import 'package:auto_route/auto_route.dart';
+
+part 'router.gr.dart';
+
+@AutoRouterConfig(replaceInRouteName: 'Page,Route')
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [];
+}
