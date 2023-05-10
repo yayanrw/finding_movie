e2976f4034d2b21d8d88172d4fab32d3c16226fe
yayanrw <yayanraw@gmail.com>
2023-05-11 06:51:47 +0700

    init constants.dart

---
+++ lib/core/config/constants.dart
@@ -0,0 +1,7 @@
+const String appTitle = 'Finding Movie';
+const String fontFamily = 'Poppins';
+const double defaultRadius = 8.0;
+const double defaultPadding = 16.0;
+const String baseUrl = 'https://api.themoviedb.org/3';
+const String imageDir = 'https://image.tmdb.org/t/p/w500';
+const String apiKey = 'api_key=2174d146bb9c0eab47529b2e77d6b526';
