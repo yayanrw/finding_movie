da43c807fe00012376c4d7745d7a5cb175c0203a
yayanrw <yayanraw@gmail.com>
2023-05-11 13:48:37 +0700

    update constants.dart value

--- lib/core/config/constants.dart
+++ lib/core/config/constants.dart
@@ -4,4 +4,4 @@ const double defaultRadius = 8.0;
 const double defaultPadding = 16.0;
 const String baseUrl = 'https://api.themoviedb.org/3';
 const String imageDir = 'https://image.tmdb.org/t/p/w500';
-const String apiKey = 'api_key=2174d146bb9c0eab47529b2e77d6b526';
+const String bearerToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNWQ3ZDcwN2M1ZWY1NWZiNWI4MjhhNDE2ZmEzZGNiOSIsInN1YiI6IjYyNzI5ZjFhYTMxM2I4MTJjYmRlZDk4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.miNA7aldD4w3I3oIQRPaE0OXOZukXrThif8lyUkt5ZM';
