904913f13170ff6e687011c0a2b6b0030fc4b4a4
yayanrw <yayanraw@gmail.com>
2023-05-13 16:02:57 +0700

    change to named params

--- lib/domain/entities/movies/genre.dart
+++ lib/domain/entities/movies/genre.dart
@@ -3,7 +3,7 @@ import 'package:equatable/equatable.dart';
 class Genre extends Equatable {
   final String name;
 
-  const Genre(this.name);
+  const Genre({required this.name});
 
   @override
   List<Object?> get props => [name];
