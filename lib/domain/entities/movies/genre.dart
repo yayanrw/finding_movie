c466929de8b1ef8e422c447f2d1191d46f9d50eb
yayanrw <yayanraw@gmail.com>
2023-05-13 16:44:53 +0700

    add extension toEntity()

--- lib/domain/entities/movies/genre.dart
+++ lib/domain/entities/movies/genre.dart
@@ -1,4 +1,5 @@
 import 'package:equatable/equatable.dart';
+import 'package:finding_movie/data/datasources/remote/dtos/movies/genre_dto.dart';
 
 class Genre extends Equatable {
   final String name;
@@ -7,4 +8,10 @@ class Genre extends Equatable {
 
   @override
   List<Object?> get props => [name];
+}
+
+extension GenreDtoExtension on GenreDto {
+  Genre toEntity() {
+    return Genre(name: name);
+  }
 }
