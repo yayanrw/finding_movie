52d2ed278f34679b8291b97a4419222ddc95ae34
yayanrw <yayanraw@gmail.com>
2023-05-13 18:11:39 +0700

    change font

--- lib/core/config/constants.dart
+++ lib/core/config/constants.dart
@@ -1,5 +1,5 @@
 const String appTitle = 'Finding Movie';
-const String fontFamily = 'Poppins';
+const String fontFamily = 'Lato';
 const double defaultRadius = 8.0;
 const double defaultPadding = 16.0;
 const String baseUrl = 'https://api.themoviedb.org/3';
