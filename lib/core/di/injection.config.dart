1d6962e3a6fc6c22bfbdbe2956fcc187bee3657c
yayanrw <yayanraw@gmail.com>
2023-05-13 17:06:30 +0700

    add generated di

--- lib/core/di/injection.config.dart
+++ lib/core/di/injection.config.dart
@@ -13,8 +13,13 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:http/http.dart' as _i4;
 import 'package:injectable/injectable.dart' as _i2;
 
+import '../../data/datasources/remote/remote_datasource.dart' as _i5;
+import '../../data/repositories/movie_repository_impl.dart' as _i9;
+import '../../data/repositories/tv_repository_impl.dart' as _i7;
+import '../../domain/repositories/movie_repository.dart' as _i8;
+import '../../domain/repositories/tv_repository.dart' as _i6;
 import '../router/router.dart' as _i3;
-import 'register_module.dart' as _i5;
+import 'register_module.dart' as _i10;
 
 extension GetItInjectableX on _i1.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
@@ -30,8 +35,14 @@ extension GetItInjectableX on _i1.GetIt {
     final registerModule = _$RegisterModule();
     gh.singleton<_i3.AppRouter>(registerModule.appRouter);
     gh.lazySingleton<_i4.Client>(() => registerModule.httpClient);
+    gh.lazySingleton<_i5.RemoteDataSource>(
+        () => _i5.RemoteDataSourceImpl(gh<_i4.Client>()));
+    gh.lazySingleton<_i6.TvRepository>(
+        () => _i7.TvRepositoryImpl(gh<_i5.RemoteDataSource>()));
+    gh.lazySingleton<_i8.MovieRepository>(() =>
+        _i9.MovieRepositoryImpl(remoteDataSource: gh<_i5.RemoteDataSource>()));
     return this;
   }
 }
 
-class _$RegisterModule extends _i5.RegisterModule {}
+class _$RegisterModule extends _i10.RegisterModule {}
