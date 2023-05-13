b1e6a42c62394876732b0ec76b68dc8f310f18d0
yayanrw <yayanraw@gmail.com>
2023-05-13 18:15:24 +0700

    support id lang

--- lib/generated/intl/messages_all.dart
+++ lib/generated/intl/messages_all.dart
@@ -17,16 +17,20 @@ import 'package:intl/message_lookup_by_library.dart';
 import 'package:intl/src/intl_helpers.dart';
 
 import 'messages_en.dart' as messages_en;
+import 'messages_id.dart' as messages_id;
 
 typedef Future<dynamic> LibraryLoader();
 Map<String, LibraryLoader> _deferredLibraries = {
   'en': () => new SynchronousFuture(null),
+  'id': () => new SynchronousFuture(null),
 };
 
 MessageLookupByLibrary? _findExact(String localeName) {
   switch (localeName) {
     case 'en':
       return messages_en.messages;
+    case 'id':
+      return messages_id.messages;
     default:
       return null;
   }
