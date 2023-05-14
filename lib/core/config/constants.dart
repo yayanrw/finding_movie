ff38f64683989e486a7c7be74576aaa79c843e2e
yayanrw <yayanraw@gmail.com>
2023-05-14 18:07:33 +0700

    change value

--- lib/core/config/constants.dart
+++ lib/core/config/constants.dart
@@ -1,7 +1,8 @@
 const String appTitle = 'Finding Movie';
 const String fontFamily = 'Lato';
-const double defaultRadius = 8.0;
-const double defaultPadding = 28.91;
+const double defaultRadius = 32;
+const double secondRadius = 22;
+const double defaultPadding = 26;
 const String baseUrl = 'https://api.themoviedb.org/3';
 const String imageDir = 'https://image.tmdb.org/t/p/w500';
 const String bearerToken = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlNWQ3ZDcwN2M1ZWY1NWZiNWI4MjhhNDE2ZmEzZGNiOSIsInN1YiI6IjYyNzI5ZjFhYTMxM2I4MTJjYmRlZDk4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.miNA7aldD4w3I3oIQRPaE0OXOZukXrThif8lyUkt5ZM';
