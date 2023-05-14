93728549a34c63a72e4239e41ff5cb5401ce0a80
yayanrw <yayanraw@gmail.com>
2023-05-15 06:20:26 +0700

    use providers

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:finding_movie/core/config/constants.dart';
 import 'package:finding_movie/core/router/router.dart';
 import 'package:finding_movie/core/theme/my_themes.dart';
 import 'package:finding_movie/generated/l10n.dart';
+import 'package:finding_movie/presentation/pages/home/home_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:finding_movie/core/di/injection.dart' as di;
 import 'dart:ui' as ui;
@@ -25,19 +26,24 @@ class MyApp extends StatelessWidget {
 
     S.load(deviceLocale);
 
-    return MaterialApp.router(
-      useInheritedMediaQuery: true,
-      title: appTitle,
-      debugShowCheckedModeBanner: false,
-      theme: myThemes(),
-      routerConfig: _appRouter.config(),
-      localizationsDelegates: const [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => di.getIt<HomeNotifier>()),
       ],
-      supportedLocales: S.delegate.supportedLocales,
+      child: MaterialApp.router(
+        useInheritedMediaQuery: true,
+        title: appTitle,
+        debugShowCheckedModeBanner: false,
+        theme: myThemes(),
+        routerConfig: _appRouter.config(),
+        localizationsDelegates: const [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+      ),
     );
   }
 }
