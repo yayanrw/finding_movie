d023f282bcee77a36e9e51bc8a0dd8d56a6a745d
yayanrw <yayanraw@gmail.com>
2023-05-15 23:37:23 +0700

    add title

--- lib/presentation/widgets/detail/image_card_widget.dart
+++ lib/presentation/widgets/detail/image_card_widget.dart
@@ -1,12 +1,18 @@
 import 'package:finding_movie/core/config/constants.dart';
+import 'package:finding_movie/core/theme/my_text_theme.dart';
 import 'package:flutter/material.dart';
 
 class ImageCardWidget extends StatelessWidget {
-  const ImageCardWidget({Key? key, required this.onTap, required this.imgUrl})
+  const ImageCardWidget(
+      {Key? key,
+      required this.onTap,
+      required this.imgUrl,
+      required this.title})
       : super(key: key);
 
   final VoidCallback onTap;
   final String imgUrl;
+  final String title;
 
   @override
   Widget build(BuildContext context) {
@@ -14,21 +20,32 @@ class ImageCardWidget extends StatelessWidget {
       padding: const EdgeInsets.only(right: 18),
       child: GestureDetector(
         onTap: onTap,
-        child: SizedBox(
-          width: 154,
-          height: 114,
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(defaultRadius),
-            child: Stack(
-              fit: StackFit.expand,
-              children: [
-                Image.network(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: 154,
+              height: 114,
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(defaultRadius),
+                child: Image.network(
                   imgUrl,
                   fit: BoxFit.cover,
                 ),
-              ],
+              ),
             ),
-          ),
+            const SizedBox(
+              height: 14,
+            ),
+            SizedBox(
+              width: 154,
+              child: Text(
+                title,
+                style: myTextTheme().labelMedium,
+                maxLines: 2,
+              ),
+            ),
+          ],
         ),
       ),
     );
